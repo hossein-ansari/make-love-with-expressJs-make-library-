349753898c49bb793fbaa288c9f6c495687cfd46
hossein-ansari <hossein838485@gmail.com>
2024-05-14 17:54:15 +0330

    get one and all books

--- controller/bookController.js
+++ controller/bookController.js
@@ -1,6 +1,7 @@
 const bookModel = require("../models/book");
+const {isValidObjectId} = require("mongoose")
 const Validator = require("../validator/booksValidator");
-exports.create = async (req,res) =>{
+exports.create = async (req, res) => {
   const validatorRes = Validator(req.body);
   if (validatorRes !== true) {
     res.status(422).json(validatorRes);
@@ -13,4 +14,20 @@ exports.create = async (req,res) =>{
   res.status(200).json({
     massage: "user created",
   });
-}
+};
+exports.getAll = async (req, res) => {
+  const books = await bookModel.find({});
+  res.status(200).json(books);
+};
+exports.getOne = async (req, res) => {
+  const { id } = req.params;
+  const idValidator = isValidObjectId(id);
+  if (!idValidator) {
+    return res.status(404).json({ error: "Book not found" });
+  }
+  const book = await bookModel.findById(id);
+  if (!book) {
+    return res.status(404).json({ error: "Book not found" });
+  }
+  res.status(200).json(book);
+};
--- router/books.js
+++ router/books.js
@@ -4,12 +4,11 @@ const bookModel = require("../models/book");
 const Validator = require("../validator/booksValidator");
 const userController = require('../controller/bookController')
 
-
 booksRouters.post("/create",userController.create )
-booksRouters.post("/getOne",userController.getOne )
-booksRouters.post("/getAll",userController.getAll )
-booksRouters.post("/deleteOne",userController.deleteOne )
-booksRouters.post("/updateOne",userController.updateOne )
+booksRouters.get("/getOne/:id",userController.getOne )
+booksRouters.get("/getAll",userController.getAll )
+// booksRouters.delete("/deleteOne",userController.deleteOne )
+// booksRouters.delete("/updateOne",userController.updateOne )
 
 
 module.exports = booksRouters;
